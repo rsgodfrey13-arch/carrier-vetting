00541a262e1f4f89729fcbf72cad1c73b5f05037
rsgodfrey13-arch <rsgodfrey13@gmail.com>
2025-12-21 16:19:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -43,6 +43,13 @@ const pool = new Pool({
   ssl: { rejectUnauthorized: false }
 });
 
+const fs = require("fs");
+
+if (process.env.GCP_SA_KEY_B64) {
+  const keyPath = process.env.GOOGLE_APPLICATION_CREDENTIALS || "/tmp/gcp-key.json";
+  fs.writeFileSync(keyPath, Buffer.from(process.env.GCP_SA_KEY_B64, "base64"));
+  process.env.GOOGLE_APPLICATION_CREDENTIALS = keyPath;
+}
 
 
 // PDF Parse normalizer
