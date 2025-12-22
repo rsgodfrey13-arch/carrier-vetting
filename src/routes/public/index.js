42994524fef23065a10f35f9eaf6b4c0942fb688
rsgodfrey13-arch <rsgodfrey13@gmail.com>
2025-12-22 17:31:15 -0500

    Create index.js

---
+++ src/routes/public/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const express = require("express");
+
+// These files will be created next (can be placeholders for now)
+const contractPublicRoutes = require("./contractPublic.routes");
+const prettyDotRoutes = require("./prettyDot.routes");
+
+function publicRoutes() {
+  const router = express.Router();
+
+  router.use(contractPublicRoutes);
+  router.use(prettyDotRoutes);
+
+  return router;
+}
+
+module.exports = { publicRoutes };
