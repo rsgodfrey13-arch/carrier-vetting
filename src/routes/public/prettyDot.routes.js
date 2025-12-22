f97125c858b25e3300cef583a00d9da4d16ff02b
rsgodfrey13-arch <rsgodfrey13@gmail.com>
2025-12-22 18:36:17 -0500

    Update prettyDot.routes.js

--- src/routes/public/prettyDot.routes.js
+++ src/routes/public/prettyDot.routes.js
@@ -1,6 +1,16 @@
 "use strict";
+
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
+
+/**
+ * PRETTY URL: /12345 → serve carrier.html
+ * This must be AFTER /api/* routes (it is, because /api is mounted separately)
+ */
+router.get("/:dot(\\d+)", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../../static", "carrier.html"));
+});
 
 module.exports = router;
