d6850936db9471b0ff9a6a8a0d56fcb86f0634ee
rsgodfrey13-arch <rsgodfrey13@gmail.com>
2025-12-23 07:17:46 -0500

    Update server.js

--- src/app/server.js
+++ src/app/server.js
@@ -7,6 +7,10 @@ const { createApp } = require("./app");
 const app = createApp();
 const port = process.env.PORT || 3000;
 
+const { externalV1Routes } = require("../routes/external/v1.routes");
+app.use("/api/v1", externalV1Routes());
+
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
