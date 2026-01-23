9f14d6e42dd00245892fe84fab0a9ba3d10882e5
rsgodfrey13-arch <rsgodfrey13@gmail.com>
2026-01-22 19:17:10 -0500

    Update index.js

--- src/routes/public/index.js
+++ src/routes/public/index.js
@@ -5,6 +5,7 @@ const express = require("express");
 // These files will be created next (can be placeholders for now)
 const contractPublicRoutes = require("./contractPublic.routes");
 const prettyDotRoutes = require("./prettyDot.routes");
+const healthzRoutes = require("./healthz.routes"); // 👈 ADD
 
 function publicRoutes() {
   const router = express.Router();
