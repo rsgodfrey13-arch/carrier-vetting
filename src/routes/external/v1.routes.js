f48238926c26a19bfcdfb8463fee80ebf6526923
rsgodfrey13-arch <rsgodfrey13@gmail.com>
2026-01-22 19:39:27 -0500

    Update v1.routes.js

--- src/routes/external/v1.routes.js
+++ src/routes/external/v1.routes.js
@@ -4,15 +4,18 @@ const express = require("express");
 const { apiAuth } = require("../../middleware/apiAuth");
 const { pool } = require("../../db/pool");
 const createApiV1 = require("./v1.router");
+const { healthExternalRoutes } = require("./external/healthExternal.routes");
 
 // IMPORT YOUR DOCUPIPE ROUTES
 const docupipeRoutes = require("./docupipe.routes");
+
 
 function externalV1Routes() {
   const router = express.Router();
 
   // Webhook FIRST (no apiAuth)
   router.use(docupipeRoutes);
+  router.use(healthExternalRoutes); 
 
   // Everything else protected
   router.use(apiAuth);
