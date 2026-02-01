b37f84dd814edd9dc26315b7ab6b17450f8be72c
rsgodfrey13-arch <rsgodfrey13@gmail.com>
2026-02-01 12:34:39 -0500

    Update account.routes.js

--- src/routes/public/account.routes.js
+++ src/routes/public/account.routes.js
@@ -3,6 +3,11 @@
 const express = require("express");
 const path = require("path");
 
+
+// Do this when im ready for auth to be requred 
+// const requireAuth = require("../internal/auth.middleware");
+
+
 function accountRoutes() {
   const router = express.Router();
 
