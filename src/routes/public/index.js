4baa938db88c58470be72aa62483c72b18662f39
rsgodfrey13-arch <rsgodfrey13@gmail.com>
2026-02-02 20:30:59 -0500

    Update index.js

--- src/routes/public/index.js
+++ src/routes/public/index.js
@@ -8,13 +8,15 @@ const prettyDotRoutes = require("./prettyDot.routes");
 const accountRoutes = require("./account.routes");
 const healthzRoutes = require("./healthz.routes"); 
 const trackRoutes = require("./track"); // adjust name/path if needed
+const resetPasswordRoutes = require("./resetPassword.routes");
 
 function publicRoutes() {
   const router = express.Router();
   router.use(contractPublicRoutes);
   router.use(healthzRoutes); 
   router.use(trackRoutes);   
-  router.use(accountRoutes);    // 👈 static page route first
+  router.use(accountRoutes);
+  router.use(resetPasswordRoutes);
   router.use(prettyDotRoutes);  // 👈 wildcard-ish routes last
   return router;
 }
