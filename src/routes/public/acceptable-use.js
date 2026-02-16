b82724fca5b12745ea5e998514c52050d47f95cb
rsgodfrey13-arch <rsgodfrey13@gmail.com>
2026-02-16 18:00:35 -0500

    Create acceptable-use.js

---
+++ src/routes/public/acceptable-use.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const express = require("express");
+const path = require("path");
+
+const router = express.Router();
+
+router.get("/account", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../../static", "acceptable-use.html"));
+});
+
+module.exports = router;
