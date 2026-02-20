454b5686d1eb2823985b6bbaf67ee3f404748f20
rsgodfrey13-arch <rsgodfrey13@gmail.com>
2026-02-19 21:59:12 -0500

    Update demo.routes.js

--- src/routes/public/demo.routes.js
+++ src/routes/public/demo.routes.js
@@ -1,1 +1,12 @@
+"use strict";
 
+const express = require("express");
+const path = require("path");
+
+const router = express.Router();
+
+router.get("/demo", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../../static", "demo.html"));
+});
+
+module.exports = router;
