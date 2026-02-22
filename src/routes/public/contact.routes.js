503b50c781dd9793fccc158faafb5bfae6234824
rsgodfrey13-arch <rsgodfrey13@gmail.com>
2026-02-22 11:47:44 -0500

    Add contact routes to serve contact.html

---
+++ src/routes/public/contact.routes.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const express = require("express");
+const path = require("path");
+
+const router = express.Router();
+
+router.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../../static", "contact.html"));
+});
+
+module.exports = router;
