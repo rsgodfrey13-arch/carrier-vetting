c4d553a9abb67532297f3894ea0fb55caafdbe75
rsgodfrey13-arch <rsgodfrey13@gmail.com>
2026-03-03 19:14:11 -0500

    Update contracts.routes.js

--- src/routes/internal/contracts.routes.js
+++ src/routes/internal/contracts.routes.js
@@ -6,11 +6,12 @@ const crypto = require("crypto");
 const { pool } = require("../../db/pool");
 const { requireAuth } = require("../../middleware/requireAuth");
 const { loadCompanyContext } = require("../../middleware/companyContext");
-const { sendTeamInviteEmail } = require("../../clients/mailgun"); // adjust if different
+const { sendContractEmail } = require("../../clients/mailgun");
+
+const { spaces } = require("../../clients/spacesS3v2");
+
 
 const router = express.Router();
-
-const INVITE_EXPIRES_HOURS = 72;;
 
 function makeToken() {
   return crypto.randomBytes(24).toString("hex");
