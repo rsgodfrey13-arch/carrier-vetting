f2718f4a5494c01dba9efae8ff9f156a53558848
rsgodfrey13-arch <rsgodfrey13@gmail.com>
2026-03-13 19:15:21 -0400

    Update getting-started.routes.js

--- src/routes/public/help/getting-started.routes.js
+++ src/routes/public/help/getting-started.routes.js
@@ -6,7 +6,7 @@ const path = require("path");
 const router = express.Router();
 
 router.get("/help/getting-started", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../../static/help", "getting-started.html"));
+  res.sendFile(path.join(__dirname, "../../../../static/help", "getting-started.html"));
 });
 
 module.exports = router;
